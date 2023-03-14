a64733ea3f32ab1be55dbea44ce06f8ee21bb0dd
Pere1J <pere1josep@gmail.com>
2023-03-14 14:50:15 +0100

    added information to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,6 +8,65 @@ INTO
   (name, nacionalidad)
 VALUES
   ('Miguel de Cervantes', 'España');
+
+ INSERT
+ INTO
+   authors
+   (name, nacionalidad)
+ VALUES
+   ('Jane Austen', 'Inglaterra');
+INSERT
+INTO
+  authors
+  (name, nacionalidad)
+VALUES
+  ('Hergè', 'Bélgica');
+
+INSERT
+INTO
+  authors
+  (name, nacionalidad)
+VALUES
+  ('Howard', 'Estados Unidos');
+INSERT
+INTO
+  authors
+  (name, nacionalidad)
+VALUES
+  ('Bram Stocker', 'Irlanda');
+INSERT
+INTO
+  authors
+  (name, nacionalidad)
+VALUES
+  ('Edgar Rice Burroughs', 'Estados Unidos');
+
+INSERT
+INTO
+  authors
+  (name, nacionalidad)
+VALUES
+  ('Karl Marx', 'Alemania');
+
+INSERT
+INTO
+  authors
+  (name, nacionalidad)
+VALUES
+  ('Sandra Moya', 'España');
+INSERT
+INTO
+  authors
+  (name, nacionalidad)
+VALUES
+  ('Sara Mesa', 'Argentina');
+INSERT
+INTO
+  authors
+  (name, nacionalidad)
+VALUES
+  ('Walter Scott', 'Escocia');
+
 INSERT
 INTO
   books
@@ -20,3 +79,58 @@ INTO
     (title, is_loaned, cover_url, author_id)
 VALUES
     ('Novelas ejemplares', FALSE, 'https://editorialverbum.es/wp-content/uploads/2016/11/novelasejemplares.jpg', 1);
+INSERT
+INTO
+    books
+    (title, is_loaned, cover_url, author_id)
+VALUES
+    ('Emma', TRUE, 'https://kbimages1-a.akamaihd.net/1ea81b28-2fa1-4796-b9b0-710123b48227/353/569/90/False/emma-503.jpg', 2);
+    INSERT
+    INTO
+        books
+        (title, is_loaned, cover_url, author_id)
+VALUES
+        ('Tintín', TRUE, 'https://papersgirona.com/wp-content/webpc-passthru.php?src=https://papersgirona.com/wp-content/uploads/2020/07/IMG_0022.jpg&nocache=1', 3);
+ INSERT
+INTO
+            books
+            (title, is_loaned, cover_url, author_id)
+VALUES
+            ('Conan', FALSE, 'https://m.media-amazon.com/images/I/712C4wexOxL.jpg', 4);
+INSERT
+INTO
+   books
+ (title, is_loaned, cover_url, author_id)
+VALUES
+ ('Drácula', TRUE, 'https://imagessl6.casadellibro.com/a/l/t7/36/9788415618836.jpg', 5);
+INSERT
+INTO
+                    books
+                    (title, is_loaned, cover_url, author_id)
+VALUES
+ ('Tarzán', FALSE, 'https://pictures.abebooks.com/isbn/9780307131942-es.jpg', 6);
+
+ INSERT
+ INTO
+     books
+     (title, is_loaned, cover_url, author_id)
+ VALUES
+     ('El Capital', TRUE, 'https://pictures.abebooks.com/inventory/md/md30822975656.jpg', 7);
+INSERT
+INTO
+    books
+    (title, is_loaned, cover_url, author_id)
+VALUES
+    ('Aurora', TRUE, 'https://f7099cdac3ad9a351ae3-74c3dcaca50fe6cfec786b085d2f6908.ssl.cf5.rackcdn.com/books/images/lantia_cover_622af53a3dd25_360.jpg', 8);
+INSERT
+INTO
+    books
+    (title, is_loaned, cover_url, author_id)
+VALUES
+    ('Un amor', FALSE, 'https://www.anagrama-ed.es/uploads/media/portadas/0001/25/4de1878d73446124ee5b4e6c48b6f0fee6d8115a.jpeg', 9);
+INSERT
+INTO
+    books
+    (title, is_loaned, cover_url, author_id)
+VALUES
+    ('Ivanhoe', TRUE, 'https://images.cdn2.buscalibre.com/fit-in/360x360/b5/bc/b5bcde278d2b5a97eb4b492c3361abec.jpg', 10);
