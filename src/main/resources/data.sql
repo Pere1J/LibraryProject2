48f4983c6ec3dd583b1d9161c6ba9e484961d7a0
Pere1J <pere1josep@gmail.com>
2023-03-14 13:56:30 +0100

    refined sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,10 +13,10 @@ INTO
   books
   (title, is_loaned, cover_url, author_id)
 VALUES
-  ('El Quijote', FALSE, 'quijote.jpg', 1);
+  ('El Quijote', FALSE, 'https://www.popularlibros.com/imagenes.webp_grandes/9788466/978846623664.webp', 1);
 INSERT
 INTO
     books
     (title, is_loaned, cover_url, author_id)
 VALUES
-    ('Novelas ejemplares', FALSE, 'novelas.jpg', 1);
+    ('Novelas ejemplares', FALSE, 'https://editorialverbum.es/wp-content/uploads/2016/11/novelasejemplares.jpg', 1);
