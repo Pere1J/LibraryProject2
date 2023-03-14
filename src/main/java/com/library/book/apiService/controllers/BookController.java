0ecd7fca1c66279eab424999f91105d55e4296d0
Pere1J <pere1josep@gmail.com>
2023-03-14 14:23:13 +0100

    added Crossorigin

--- src/main/java/com/library/book/apiService/controllers/BookController.java
+++ src/main/java/com/library/book/apiService/controllers/BookController.java
@@ -3,12 +3,14 @@ package com.library.book.apiService.controllers;
 import com.library.book.domain.models.Book;
 import com.library.book.domain.services.BookService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 
 public class BookController {
     private final BookService bookService;
