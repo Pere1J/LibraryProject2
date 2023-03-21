6958e3e60269369b9b7697eb89081cd4dfdb91c1
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-03-21 13:03:31 +0100

    add CossOrigin in Author Controller

--- src/main/java/com/library/book/apiService/controllers/AuthorController.java
+++ src/main/java/com/library/book/apiService/controllers/AuthorController.java
@@ -2,6 +2,7 @@ package com.library.book.apiService.controllers;
 
 import com.library.book.domain.models.Author;
 import com.library.book.domain.services.AuthService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/authors")
 public class AuthorController {
     private final AuthService authService;
