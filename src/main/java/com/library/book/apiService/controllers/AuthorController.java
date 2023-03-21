8bf92f9e1ef4aef0e03f799407da790ae583de18
Pere1J <pere1josep@gmail.com>
2023-03-21 12:34:46 +0100

    add author controller and endpoint

---
+++ src/main/java/com/library/book/apiService/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package com.library.book.apiService.controllers;
+
+import com.library.book.domain.models.Author;
+import com.library.book.domain.services.AuthService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/authors")
+public class AuthorController {
+    private final AuthService authService;
+
+    public AuthorController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @GetMapping
+    public List<Author> findAllAuthors(){
+        return authService.findAllAuthors();
+    }
+}
--- src/main/java/com/library/book/domain/services/AuthService.java
+++ src/main/java/com/library/book/domain/services/AuthService.java
@@ -5,6 +5,8 @@ import com.library.book.infrastructure.repositories.IAuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AuthService {
     @Autowired
@@ -14,5 +16,9 @@ public class AuthService {
         return this.authorRepository.findById(1L).get();
 
     };
+
+    public List<Author> findAllAuthors(){
+        return authorRepository.findAll();
+    }
 
 }
