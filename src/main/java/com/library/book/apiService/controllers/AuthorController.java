a681d82e18051de26491e49ed0444de2910433ca
Pere1J <pere1josep@gmail.com>
2023-03-22 10:05:35 +0100

    change origins in crossorigin

--- src/main/java/com/library/book/apiService/controllers/AuthorController.java
+++ src/main/java/com/library/book/apiService/controllers/AuthorController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("/authors")
 public class AuthorController {
     private final AuthService authService;
